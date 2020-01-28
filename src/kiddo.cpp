8200160ea38d9de167c364bc6d248a2fb05770bc
guodongxiaren <879231132@qq.com>
2020-01-29 00:15:05 +0800

    class thread io

--- src/kiddo.cpp
+++ src/kiddo.cpp
@@ -22,6 +22,7 @@ namespace kiddo {
 
 class Server {
 public:
+    Server():_fd_size(0) {}
     int io_td() {
         std::thread t(&kiddo::Server::io, this);
         t.join();
@@ -34,22 +35,44 @@ public:
         return 0;
     }
 
+    int listen_accept();
+
     struct Fd{
         int fd;
         int status;
     };
 
 private:
-    std::vector<Fd> _fd_queue;
+    Fd _fd_queue[100];
+    int _fd_size;
+    unsigned int _epollfd;
+    int _sockfd;
 
 };
-void Server::io() {
-    int sockfd, new_fd;
-    struct sockaddr_in server_addr;
-    struct sockaddr_in client_addr;
-    socklen_t sin_size;
 
-    if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) == -1) {
+int Server::listen_accept() {
+    int fd;
+    struct sockaddr_in client_addr;
+    socklen_t sin_size = sizeof(struct sockaddr_in);
+    if ((fd = accept(_sockfd, (struct sockaddr *)&client_addr, &sin_size)) == -2) {
+        perror("accept");
+        return 1;
+    }
+    struct epoll_event event;
+    event.data.fd = fd;
+    event.events = EPOLLIN|EPOLLET|EPOLLRDHUP;
+    epoll_ctl(_epollfd, EPOLL_CTL_ADD, fd, &event);
+    // re add socketfd
+    //struct epoll_event event;
+    event.data.fd = _sockfd;
+    event.events = EPOLLIN|EPOLLET|EPOLLRDHUP;
+    epoll_ctl(_epollfd, EPOLL_CTL_ADD, fd, &event);
+    return 0;
+}
+void Server::io() {
+    struct sockaddr_in server_addr;
+
+    if ((_sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         printf("create socket error");
         perror("socket");
         exit(1);
@@ -58,35 +81,34 @@ void Server::io() {
     server_addr.sin_port = htons(2323);
     server_addr.sin_addr.s_addr = INADDR_ANY;
     bzero(&(server_addr.sin_zero), 8);
-    if (bind(sockfd,(struct sockaddr *)&server_addr, sizeof(struct sockaddr))==-1) {
+    if (bind(_sockfd,(struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1) {
         perror("bind socket error");
         exit(1);
     }
-    if (listen(sockfd, 10)==-1) {
+    if (listen(_sockfd, 10) == -1) {
         perror("listen");
         exit(1);
     }
-    unsigned int epollfd = epoll_create(10);
-    struct epoll_event eventList[10];
+    _epollfd = epoll_create(10);
+    struct epoll_event event_list[10];
     int timeout = -1; // TODO
     while (1) {
-        sin_size = sizeof(struct sockaddr_in);
         struct epoll_event event;
-        event.data.fd = sockfd;
+        event.data.fd = _sockfd;
         event.events = EPOLLIN|EPOLLET|EPOLLRDHUP;
-        epoll_ctl(epollfd, EPOLL_CTL_ADD, sockfd, &event);
-        int ret = epoll_wait(epollfd, eventList, 10, timeout);
+        epoll_ctl(_epollfd, EPOLL_CTL_ADD, _sockfd, &event);
+        int ret = epoll_wait(_epollfd, event_list, 10, timeout);
         if (ret > 0) {
             for (int i = 0; i < ret; ++i) {
-                struct epoll_event event = eventList[i];
+                struct epoll_event event = event_list[i];
                 if (event.events & EPOLLIN) {
-                    if (event.data.fd == sockfd) {
-                        if ((new_fd = accept(sockfd,(struct sockaddr *)&client_addr, &sin_size))==-2) {
-                            perror("accept");
-                            exit(1);
-                        }
+                    if (event.data.fd == _sockfd) {
+                        listen_accept();
                     } else {
-                        // recv message
+                        Fd f;
+                        f.fd = event.data.fd;
+                        f.status = 0;
+                        _fd_queue[i++] = f;
                     }
                 }
             }
@@ -104,6 +126,8 @@ void Server::io() {
 }
 
 int main() {
+    kiddo::Server srv;
+    srv.io_td();
     
 }
 
