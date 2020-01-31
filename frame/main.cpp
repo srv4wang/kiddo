3345718ba5113c2e9f62be04351db6a0793c683e
guodongxiaren <879231132@qq.com>
2020-01-31 17:34:54 +0800

    new dir:frame is stand by framework

---
+++ frame/main.cpp
@@ -0,0 +1,11 @@
+#include "server.h"
+
+int main() {
+    //signal(SIGINT, kiddo::exit);
+    //signal(SIGQUIT, kiddo::exit);
+    kiddo::Server srv("127.0.0.1");
+    kiddo::work_fun_t echo = [](kiddo::Request& req, kiddo::Response& res){memcpy(res.data, req.data, sizeof(res.data));return 1;};
+    srv.set_work_fun(echo);
+    srv.work();
+    srv.io_td();
+}
